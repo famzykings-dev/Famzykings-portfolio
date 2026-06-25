e900e0539f0ba89f200a50c9c626df3efd252ea7
famzykings-dev <famzykings@gmail.com>
2026-06-25 11:51:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -26,6 +26,28 @@ const urlsToCache = [
   "/img/logo change.png",
   "/img/icon-192.png",
   "/img/icon-512.png",
+  
+  "/img/crypto.png",
+  "/img/crypto1.png"
+  "/img/crypto2.png"
+  "/img/crypto3.png"
+  "/img/crypto4.png",
+  "/img/crypto5.png"
+  "/img/crypto6.png"
+  "/img/crypto7.png"
+  "/img/crypto8.png"
+
+  "/img/fashion3.png"
+  "/img/fashion2.png"
+  "/img/fashion6.png"
+  "/img/fashion7.png"
+  "/img/fashion5.png"
+
+  "/img/kinghair1.png"
+  "/img/kingshair2.png"
+  "/img/kinghair3.png"
+  "/img/kinghair4.png"
+  "/img/kinghair5.png"
 
   // Project Images
   "/img/fashion.png",
